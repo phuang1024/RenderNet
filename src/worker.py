6bdf19df5211ebea15c1c2a5a921f40fc74f5b5c
Patrick Huang <huangpatrick16777216@gmail.com>
2022-06-21 23:32:35 -0700

    Renders frame.

--- src/worker.py
+++ src/worker.py
@@ -1,11 +1,17 @@
 import os
 import random
+import shutil
+import sys
 import time
 from base64 import b64decode
+from subprocess import Popen, DEVNULL, PIPE, STDOUT
 
 import config
 import conn
 from utils import get_tmp
+
+BLENDER = shutil.which("blender")
+assert BLENDER is not None
 
 
 def ensure_blend(tmpdir, job_id):
@@ -23,6 +29,19 @@ def ensure_blend(tmpdir, job_id):
     return path
 
 
+def render(tmpdir, blend, frame):
+    args = [BLENDER, "--background", blend, "--render-output", os.path.join(tmpdir, "frame"),
+        "--render-frame", str(frame)]
+    p = Popen(args, stdin=DEVNULL, stdout=PIPE, stderr=STDOUT)
+    p.wait()
+
+    if p.returncode != 0:
+        print("Blender render failed. Blender output:")
+        sys.stdout.buffer.write(p.stdout.read())
+
+    return os.path.join(tmpdir, f"frame{frame:04}.jpg")
+
+
 def start():
     tmpdir = get_tmp("worker")
     try:
@@ -36,8 +55,10 @@ def start():
 
             if resp["found"]:
                 blend = ensure_blend(tmpdir, resp["id"])
-                print(blend)
-                break  # TODO remove
+                print("Rendering: job={}, frame={}".format(resp["id"], resp["frame"]))
+                output = render(tmpdir, blend, resp["frame"])
+                print(output)
+                break
             else:
                 time.sleep(5)
 
