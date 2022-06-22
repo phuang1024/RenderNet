4775d55d7aa978eb28b0fd9b69866f6b80c6ff88
Patrick Huang <huangpatrick16777216@gmail.com>
2022-06-22 10:31:44 -0700

    Signal handler.

--- src/worker.py
+++ src/worker.py
@@ -1,6 +1,7 @@
 import os
 import random
 import shutil
+import signal
 import sys
 import time
 from base64 import b64decode, b64encode
@@ -13,6 +14,15 @@ from utils import get_tmp
 BLENDER = shutil.which("blender")
 assert BLENDER is not None
 
+RUN = True   # Changed by SIGINT handler.
+
+
+def sigint_handler(sig, frame):
+    global RUN
+    RUN = False
+
+    print("\nFinishing current jobs.")
+
 
 def ensure_blend(tmpdir, job_id):
     path = os.path.join(tmpdir, f"{job_id}.blend")
@@ -24,7 +34,6 @@ def ensure_blend(tmpdir, job_id):
                 "id": job_id,
             })
             f.write(b64decode(resp))
-
 
     return path
 
@@ -43,24 +52,32 @@ def render(tmpdir, blend, frame):
 
 
 def start():
+    global RUN
+    RUN = True
+
+    signal.signal(signal.SIGINT, sigint_handler)
+
     tmpdir = get_tmp("worker")
     try:
-        while True:
-            print("Requesting work.")
+        while RUN:
+            print("Requesting work; ", end="", flush=True)
             resp = conn.request({
                 "type": "worker",
                 "action": "request_work",
                 "accept": config.get("worker_accept"),
             })
+            print("Found" if resp["found"] else "Not found")
 
             if resp["found"]:
                 job_id = resp["id"]
                 frame = resp["frame"]
 
+                start = time.time()
+                print(f"Downloading blend; ", end="", flush=True)
                 blend = ensure_blend(tmpdir, job_id)
-                print(f"Rendering: job={job_id}, frame={frame};   ", end="", flush=True)
+                print(f"Rendering: job={job_id}, frame={frame}; ", end="", flush=True)
                 output = render(tmpdir, blend, frame)
-                print("Uploading;   ", end="", flush=True)
+                print("Uploading; ", end="", flush=True)
                 with open(output, "rb") as f:
                     conn.request({
                         "type": "worker",
@@ -69,7 +86,9 @@ def start():
                         "frame": frame,
                         "data": b64encode(f.read()).decode(),
                     })
-                print("Done")
+
+                elapse = time.time() - start
+                print(f"Done {elapse:.3f}s")
 
             else:
                 time.sleep(5)
