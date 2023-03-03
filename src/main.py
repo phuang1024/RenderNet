a0a71e37f30c8e591bd13f95b115b183ddc5ae47
Patrick Huang <huangpatrick16777216@gmail.com>
2023-03-03 07:45:27 -0800

    Create config and server start.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
 import argparse
+import json
+import os
+
+from server import Server
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(ROOT, "config.json")
+
+
+def create_config():
+    print("Creating config file.")
+    data = {
+        "ip": input("Server IP: "),
+        "port": int(input("Server port: ")),
+    }
+    with open(CONFIG_PATH, "w") as f:
+        json.dump(data, f, indent=4)
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("mode", choices=["server", "client", "worker", "config"], default="client")
+    args = parser.parse_args()
+
+    if not os.path.isfile(CONFIG_PATH) or args.mode == "config":
+        create_config()
+
+    with open(CONFIG_PATH) as f:
+        config = json.load(f)
+
+    if args.mode == "server":
+        server = Server(config["ip"], config["port"])
+        server.start()
 
 
 if __name__ == "__main__":
